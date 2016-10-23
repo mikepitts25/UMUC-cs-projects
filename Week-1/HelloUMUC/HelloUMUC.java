7d9a256a272085e45ce3fb5b291f699c2f703306
Mike Pitts <mikepitts25@gmail.com>
2016-10-22 23:55:25 -0400

    Added Week 1
    HelloUMUC Java

---
+++ Week-1/HelloUMUC/HelloUMUC.java
@@ -0,0 +1,11 @@
+/**
+* Print "Hello UMUC!" to standard output.
+*/
+public class HelloUMUC
+{
+	public static void main(String[] args)
+	{
+		// Display the String.
+		System.out.println("Hello UMUC!");
+	}
+}
